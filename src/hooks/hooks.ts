09657af4adc62f5e4bea3d424345d9b46635dcd2
John4850 <johnnelson4850@gmail.com>
2023-03-16 01:30:16 -0700

    initial setup, Picture of the Day API call

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,26 +1,23 @@
 import { useState, useEffect } from 'react';
 
-const avatarApiAddress = "https://last-airbender-api.fly.dev/api/v1/characters";
+export const useFetch = (url: string) => {
 
-export const useFetch = (url: string): object => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>();
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => {
-        setError(data.error)
-        setData(data.joke)
+      .then((JSON) => {
+        setError(JSON.error)
+        setData(JSON)
         setLoading(false)
       })
-  }, [url]);
+  }, []);
 
   return { data, loading, error };
 };
-
-export default useFetch;
 
 export function useRandomAvatarCharacterArray() {
   let [randomAvatarArray, setRandomAvatarArray] = useState([]);
