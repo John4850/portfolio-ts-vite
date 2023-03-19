f73b416d90560869c7c40abe6afa8154603e4e23
John4850 <johnnelson4850@gmail.com>
2023-03-19 00:48:47 -0700

    style changes

--- src/components/BetterFuture.tsx
+++ src/components/BetterFuture.tsx
@@ -1,13 +1,21 @@
 
 const BetterFuture: React.FC = () => (
   <>
-    <p className="alt-text">A data visual tool I built for
-      <a href="https://bffa.org/" target="_blank"> Better Future For All</a>
-    </p>
-    <iframe height="500px" width="100%" src="https://betterfutureforall.netlify.app/">
-      Your browser does not support embedded frames (iFrames)
-      <a href="https://betterfutureforall.netlify.app/" target="_blank">further</a>
-    </iframe>
+    <h3><a href="https://bffa.org/" target="_blank"> Better Future For All</a></h3>
+    <div className="project">
+      <p>Data visual tool using React, D3, and lots of JavaScript</p>
+      <iframe height="500px" width="100%" src="https://betterfutureforall.netlify.app/">
+        Your browser does not support embedded frames (iFrames)
+        <a href="https://betterfutureforall.netlify.app/" target="_blank">further</a>
+      </iframe>
+    </div>
+    <div className="project">
+      <p>React.js App for exploring the definitions and sources </p>
+      <iframe height="500px" width="100%" src="https://bffa-definitions.netlify.app/">
+        Your browser does not support embedded frames (iFrames)
+        <a href="https://betterfutureforall.netlify.app/" target="_blank">further</a>
+      </iframe>
+    </div>
   </>
 )
 export default BetterFuture;
