ad5669c99bcbb8286a32fdab5d04a727a7873697
John4850 <johnnelson4850@gmail.com>
2023-03-21 15:10:06 -0700

    bffa details added

--- src/components/BetterFuture.tsx
+++ src/components/BetterFuture.tsx
@@ -2,8 +2,14 @@
 const BetterFuture: React.FC = () => (
   <>
     <h3 className='project-header'><a href="https://bffa.org/" target="_blank"> Better Future For All 🔗</a></h3>
+    <p className="grey-text">
+      I used the 
+    <a href="https://www.socialprogress.org/framework" target="_blank"> SPI framework </a> 
+    created by 
+    <a href="https://socialprogress.org/" target="_blank"> Social Progress Imperative </a> 
+    to create intuitive tools for exploring data.</p>
     <div className="project">
-      <p>Data visual tool using React, D3, and lots of JavaScript</p>
+      <p>Choropleth map and tooltip using React, D3, and lots of JavaScript</p>
       <iframe height="500px" width="100%" src="https://betterfutureforall.netlify.app/">
         Your browser does not support embedded frames (iFrames)
         <a href="https://betterfutureforall.netlify.app/" target="_blank">further</a>
