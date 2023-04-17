7657711a7b4b3b759b03af4d06c4c0dc223787d6
John4850 <johnnelson4850@gmail.com>
2023-04-17 09:15:34 -0700

    email link

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -17,25 +17,25 @@ let email: string = 'JohnNelson4850@gmail.com';
 const ContactInfo: React.FC<Props> = ({ theme, isVisible, myRef }) => {
   return (
     <>
-    <div className='resume animate glow delay-2'>
-      <a href="https://drive.google.com/file/d/1erporqoScv8UuoXKucAd5JxeElHaMYNP/view?usp=sharing" target="_blank">
-        <img src={theme == "Dark" ? resume : resumeWhite} className={`resume-logo ${isVisible? 'animate glow delay-1' : ''}`} alt="Resume link" />
-      </a>
-    </div>
-    <div id="" className="contact-info" ref={myRef} >
-      <a href="https://github.com/John4850" target="_blank" className="contact-link">
-        <img src={theme == "Dark" ? githubMark : githubMarkWhite} className={`contact-logo ${isVisible? 'animate glow delay-2' : ''}`} alt="GitHub logo" />
-      </a>
-      <a href={`mailto:${email} subject=""  body="" `} target="_blank" className="contact-link">
-        <img src={emailLogo} className={`contact-logo ${isVisible? 'animate glow delay-3' : ''}`} alt="GitHub link" />
-      </a>
-      <a href="https://www.linkedin.com/in/johnnelson4850/" target="_blank" className="contact-link">
-        <img src={linkedInLogo} className={`contact-logo ${isVisible? 'animate glow delay-4' : ''}`} alt="LinkedIn link" />
-      </a>
-    </div>
-    <a href="https://www.instagram.com/johnnelson4850/" target="_blank">
-          <img src={instagramLogo} className={`logo ${isVisible? 'animate glow delay-5' : ''}`} alt="Instagram logo" />
+      <div className='resume animate glow delay-2'>
+        <a href="https://drive.google.com/file/d/1erporqoScv8UuoXKucAd5JxeElHaMYNP/view?usp=sharing" target="_blank">
+          <img src={theme == "Dark" ? resume : resumeWhite} className={`resume-logo ${isVisible ? 'animate glow delay-1' : ''}`} alt="Resume link" />
         </a>
+      </div>
+      <div id="" className="contact-info" ref={myRef} >
+        <a href="https://github.com/John4850" target="_blank" className="contact-link">
+          <img src={theme == "Dark" ? githubMark : githubMarkWhite} className={`contact-logo ${isVisible ? 'animate glow delay-2' : ''}`} alt="GitHub logo" />
+        </a>
+        <a href={`mailto:${email}`} target="_blank" className="contact-link">
+          <img src={emailLogo} className={`contact-logo ${isVisible ? 'animate glow delay-3' : ''}`} alt="GitHub link" />
+        </a>
+        <a href="https://www.linkedin.com/in/johnnelson4850/" target="_blank" className="contact-link">
+          <img src={linkedInLogo} className={`contact-logo ${isVisible ? 'animate glow delay-4' : ''}`} alt="LinkedIn link" />
+        </a>
+      </div>
+      <a href="https://www.instagram.com/johnnelson4850/" target="_blank">
+        <img src={instagramLogo} className={`logo ${isVisible ? 'animate glow delay-5' : ''}`} alt="Instagram logo" />
+      </a>
     </>
   )
 };
