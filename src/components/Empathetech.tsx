a22dd09a73c703b8e3f569bb3a6fa693cdbdcdb7
John4850 <johnnelson4850@gmail.com>
2023-06-11 14:39:19 -0700

    Removed iFrame

--- src/components/Empathetech.tsx
+++ src/components/Empathetech.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Empathetech: React.FC<Props> = ({ theme }) => {
 
   let getImg = <img src={theme == "Dark" ? githubMark : githubMarkWhite} className={`git-link`} alt="GitHub logo" />
-  
+
   return (
     <>
       <div className="card animate glow delay-2">
@@ -19,42 +19,40 @@ const Empathetech: React.FC<Props> = ({ theme }) => {
             <img src={theme == "Dark" ? empathetechLogo : empathetechLogoWhite} className={'logo animate svg glow delay-5'} alt="Empathetech logo" />
           </a>
         </h3>
-
-        <p className="grey-text">
-          {"(pronounced “empathetic”)"}
-          <br />
-          <br />
-          <h4>
-            {"An empathy-driven community for software engineers in every stage of their career."}
-          </h4>
-          {" We center our community through the lens of non-traditional and diverse people in tech, and we welcome all folks who believe that good software engineers are good humans."}
-        </p>
+        {"(pronounced “empathetic”)"}
 
         <div className="project">
-          <h3>{"🤗 Empathetech Community Website "}</h3>
           <p className="grey-text">
-            {"An diverse tech community who believe that good software engineers are good humans. "}
+            <h4>
+              {"An empathy-driven community for software engineers in every stage of their career."}
+            </h4>
+            {" We center our community through the lens of non-traditional and diverse people in tech,"} 
+            <br/>
+            {"and we welcome all folks who believe that good software engineers are good humans."}
+            <h4>{"Why Empathy?"}</h4>
+            {" Empathy is central to our success as software engineers. As a service-based industry, we identify and address client needs—both spoken and unspoken—through our use of technology. As a community, we share our technical knowledge openly and collaborate with peers freely. It is our authentic relationships with people that drive what we develop as software, and how we develop as professionals. Empathy is both our inspiration and our foundation."}
           </p>
-          <iframe height="650px" width="100%" src="https://empathetech-org.vercel.app/">
-            Your browser does not support embedded frames (iFrames)
-            <a href="https://empathetech-org.vercel.app/" target="_blank">further</a>
-          </iframe>
+          <a href="https://empathetech-org.vercel.app/" target="_blank">
+            <h3>{"🤗 Empathetech Community Website "}</h3>
+          </a>
           <p className="alt-text">
             {"Built using Next.JS, React, TypeScript, JavaScript, HTML, and CSS."}
           </p>
           <p className="alt-text">
             {"Collaboratively designed as an Office Hours Workshop."}
             <br />
-            <ul>
+            <h5>
               {"Personal contributions include:"}
+            </h5>
+            <ul>
               <li>
-                {"Navigation Bar,"}
+                {"Actively participate in biweekly Office Hours, practicing skills and demonstrating technologies like D3.js and TypeScript"}
               </li>
               <li>
-                {"Code of Conduct,"}
+                {"Contractor with Black Business Association of Oregon as a technical subject matter expert, providing assistance and support in tech-driven community projects."}
               </li>
               <li>
-                {"Footer"}
+                {"Collaborated on the Empathetech website using Next.js and React, playing a key role in building components like the navigation bar, footer, and code of conduct."}
               </li>
             </ul>
           </p>
